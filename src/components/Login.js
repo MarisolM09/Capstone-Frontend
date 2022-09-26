df1f0f5a6825d88d51accb039b44950ee0538014
MarisolM09 <mmendez9980@gmail.com>
2022-09-26 18:54:05 -0500

    added Login and sign-up

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,80 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
-
-
+import Button from "@mui/material/Button";
 
 export default function Login() {
+  const textField = {
+    backgroundColor: "white",
+    marginBottom: "15px",
+  };
+
   return (
     <div className="container">
-    <div className="main">
-    <form>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          required
-          label="Username"
-          defaultValue=""
-          variant="standard"
-          style ={{width: '490px'}}
-          
-        />
-        <TextField
-          required
-          id="standard-required"
-          label="Password"
-          type="password"
-          defaultValue=""
-          variant="standard"
-          style ={{width: '490px'}}
-        />
-      </Box>
-      <Button variant="contained" sx={{ color: 'black', backgroundColor: '#e0e0e0'}}>Login</Button>
-      </form>
+      <div className="main-login">
+        <div className="create-acc">
+          <p className="acc-p">Please create an account</p>
+          <p>
+            It's a simple process that will take only a minute. When you log in,
+            you can start saving hikes.
+          </p>
+          <form>
+            <TextField
+              id="outlined-basic"
+              label="First Name"
+              variant="outlined"
+              style={textField}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Last Name"
+              variant="outlined"
+              style={textField}
+            />
+            <TextField
+              id="outlined-basic"
+              label="E-mail"
+              variant="outlined"
+              style={textField}
+            />
+
+            <Button
+              variant="contained"
+              className="login-btn"
+              sx={{ color: "white", backgroundColor: "#e9a82c" }}
+            >
+              Sign-up
+            </Button>
+          </form>
+        </div>
+        <div className="login-form">
+          <form>
+            <p className="p-login">Please Login</p>
+
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              style={textField}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              type="password"
+              variant="outlined"
+              style={textField}
+            />
+
+            <Button
+              variant="contained"
+              className="login-btn"
+              sx={{ color: "white", backgroundColor: "#e9a82c" }}
+            >
+              Login
+            </Button>
+          </form>
+        </div>
+      </div>
+      <footer class="footer"></footer>
     </div>
-    <footer class="footer"></footer>
-  </div>
-  
   );
 }
